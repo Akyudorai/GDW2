ecce1d84ad1775961449bf67c332d84c4af92654
Cody Jensen <UF-CJensen@hotmail.com>
2021-10-07 15:11:54 -0400

    test

---
+++ modules/NOU/src/Entity.cpp
@@ -0,0 +1,43 @@
+/*
+NOU Framework - Created for INFR 2310 at Ontario Tech.
+(c) Samantha Stahlke 2020
+
+Entity.cpp
+Minimal and lightly OOP wrapper for the ENTT framework.
+*/
+
+#include "NOU/Entity.h"
+
+#include "../../projects/Week 4 (starter)/src/Hierarchy.h"
+using namespace OMG;
+
+namespace nou
+{
+	entt::registry Entity::ecs;
+
+	Entity Entity::Create(std::string name)
+	{			
+		entt::entity id = ecs.create();		
+		return Entity(id, name);
+	}
+
+	std::unique_ptr<Entity> Entity::Allocate(std::string name)
+	{
+		entt::entity id = ecs.create();
+		return std::move(std::make_unique<Entity>(id, name));
+	}
+
+	Entity::Entity(entt::entity id, std::string name)
+	{
+		m_id = id;
+		m_name = name;
+
+		Hierarchy::getInstance().AddEntity(this);
+	}
+
+	Entity::~Entity()
+	{
+		if(m_id != entt::null)
+			ecs.destroy(m_id);
+	}
+}
